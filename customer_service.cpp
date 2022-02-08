b70cd93f79fcd0b7b69ad29c73912c0b7d3dba31
praneeth <praneethratna@gmail.com>
2022-02-08 18:13:12 +0530

    added search dishes

--- customer_service.cpp
+++ customer_service.cpp
@@ -8,10 +8,30 @@ using namespace std;
 
 void viewdishes(){
     cout << "Dishes available are: " << endl;
-    cout << std::left << std::setw(20) << "Dish name" << internal << setw(20) << "Price" <<setw(20) << "Quantity Available"<< endl;
+    cout << "====================="<<endl;
+    cout << std::left << std::setw(20) << "Dish name" << internal
+    << setw(20) << "Price" <<setw(20) << "Quantity Available"<< endl;
     for(int i=0;i<dishes.size();i++){
-        cout << std::left << std::setw(20) << dishes[i].name << internal << setw(20) << dishes[i].price
-        << internal << setw(20) << dishes[i].quantity << endl;
+        cout << std::left << std::setw(20) << dishes[i].name << internal << setw(20)
+        << dishes[i].price << internal << setw(20) << dishes[i].quantity << endl;
+    }
+}
+void searchdish(){
+    string dishname;
+    bool notfound = true;
+    cout << "Enter the dish name to search: ";
+    cin >> dishname;
+    for(int i=0;i<dishes.size();i++){
+        if(lower(dishes[i].name) == lower(dishname)){
+            cout << "The dish that you are searching for is available to order and its details are shown below:"<<endl;
+            cout << "Price: " << dishes[i].price << endl;
+            cout << "Quantity Available: "<< dishes[i].quantity << endl;
+            notfound = false;
+            break;
+        }
+    }
+    if(notfound){
+        cout << "The dish that you are searching is not available to order due to insufficient quantity!"<<endl;
     }
 
 
@@ -28,28 +48,32 @@ void customerservices(){
     while(exit == false){
     cout << "Choose any one of the options below:" << endl;
     cout << "1. View available dishes and quantity" << endl;
-    cout << "2. Select dishes into cart" << endl;
-    cout << "3. View dishes in cart and total amount to be paid" << endl;
-    cout << "4. Place order" << endl;
-    cout << "5. Vote for dishes"<<endl;
-    cout << "6. Logout" << endl;
+    cout << "2. Search for a particular dish using its name"<<endl;
+    cout << "3. Select dishes into cart" << endl;
+    cout << "4. View dishes in cart and total amount to be paid" << endl;
+    cout << "5. Place order" << endl;
+    cout << "6. Vote for dishes"<<endl;
+    cout << "7. Logout" << endl;
     cin >> num;
     if(num == 1){
         viewdishes();
     }
     if(num == 2){
-        add_dishes();
+        searchdish();
     }
     if(num == 3){
-        viewbill();
+        add_dishes();
     }
     if(num == 4){
-        placeorder();
+        viewbill();
     }
     if(num == 5){
-        votedishes();
+        placeorder();
     }
     if(num == 6){
+        votedishes();
+    }
+    if(num == 7){
 
         cout << "You are successfully logged out" << endl;
         exit = true;
